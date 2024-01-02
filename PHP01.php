e95d7f958f9b59a9ed666b277b4ca23cda20cf29
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2024-01-02 17:59:36 +0100

    Osnove sintakse PHP-a

---
+++ PHP01.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+
+    <?= 
+     'Hello World' 
+    ?>
+</h1>
+<p>My first paragraph.</p>
+
+<?php
+        // komentar 1
+        # komentar 2
+        /*
+            komentar s 
+            više redova
+
+        */
+        $x = 10;
+        $y = 5;
+
+        echo '<p>' . $x . ',' . $y . '</p>';
+    ?>
+
+</body>
+</html>
