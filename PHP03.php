d422fb5ad1e099fbd09fb6d7f1d6ecf8ca169237
dljubicic2 <131760835+dljubicic2@users.noreply.github.com>
2024-01-11 20:40:57 +0100

    if, else , elseif / else if

---
+++ PHP03.php
@@ -0,0 +1,30 @@
+<?php
+
+
+// IF / ELSE / ELSEIF / ELSE IF
+
+
+// IF
+$socre = 95;
+if($score > 90){
+    echo 'A';
+}
+
+// ELSE
+if($score > 90){
+    echo 'B';
+}else{
+    echo 'C';
+}
+
+// ELSEIF / ELSE IF 
+
+if($score > 100){
+    echo 'D';
+}else if ($score > 80){
+    echo 'F';
+}
+else{
+    echo 'E'; 
+}
+
